178671f2358dfa069bd4cc05899b5fbfe4c7657a
Tomas Ortiz <tomasortiz9598@gmail.com>
2020-09-19 20:01:35 -0300

    Primer entrega

---
+++ 06-CalcInfManual/parser.c
@@ -0,0 +1,68 @@
+#include "parser.h"
+
+bool parser()
+{
+  Estado stateParser = P_INIT;
+  Token token;
+  bool resultado;
+  while ((token = GetNextToken()) != T_EOF)
+  {
+
+    switch (stateParser)
+    {
+    case P_INIT:
+      switch (token)
+      {
+      case T_CONSTANTE:
+        stateParser = P_CONSTANTE;
+        resultado = true;
+        break;
+      case T_IDENTIFICADOR:
+        stateParser = P_IDENTIFICADOR;
+        resultado = true;
+        break;
+      default:
+        stateParser = P_INVALIDO;
+        resultado = false;
+        break;
+      }
+      break;
+    case P_IDENTIFICADOR:
+    case P_CONSTANTE:
+      switch (token)
+      {
+      case T_SUMA:case T_MULTIPLICADOR:
+        stateParser = P_OPERADOR;
+        resultado = true;
+        break;
+      default:
+        stateParser = P_INVALIDO;
+        resultado = false;
+        break;
+      }
+      break;
+    case P_OPERADOR:
+      switch (token)
+      {
+      case T_CONSTANTE:
+        stateParser = P_CONSTANTE;
+        resultado = true;
+        break;
+      case T_IDENTIFICADOR:
+        stateParser = P_IDENTIFICADOR;
+        resultado = true;
+        break;
+      default:
+        resultado = false;
+        stateParser = P_INVALIDO;
+        break;
+      }
+      break;
+
+    default:
+      break;
+    }
+  }
+
+  return resultado;
+}
---
+++ 06-CalcInfManual/scanner.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "scanner.h"
+
+Token GetNextToken(){
+    int c;
+    c = getchar();
+    if (esMultiplicacion(c))
+        return T_MULTIPLICADOR;
+    if (esSuma(c))
+        return T_SUMA;
+    if (esEOF(c))
+        return T_EOF;
+    if (esLetra(c)){
+        leerId();
+        return T_IDENTIFICADOR;
+    }
+    if (esDigito(c)){
+        leerCte();
+        return T_IDENTIFICADOR;
+    }
+    errorLexico(c);
+    return T_INVALIDO;
+}
+
+bool esMultiplicacion(const int c){
+   return c=='*';
+}
+bool esSuma(const int c){
+    return c=='+';
+}
+bool esEOF(const int c){
+    return c==EOF;
+}
+bool esLetra(const int c){
+    return isalpha(c);
+}
+bool esDigito(const int c){
+    return isdigit(c);
+}
+
+void leerId(){
+    int c;
+    c = getchar();
+    if (esEOF(c)){
+        ungetc(c, stdin);
+        return;
+    }
+    else if (esLetra(c))
+        leerId();
+    else if (esDigito(c))
+        leerId();
+    else{
+        ungetc(c, stdin);
+        return;
+    }    
+
+}
+void leerCte(){
+    int c ;
+    c = getchar();
+    if (esEOF(c)){
+        ungetc(c, stdin);
+        return;
+    }
+    else if (esDigito(c))
+        leerCte();
+    else{
+        ungetc(c, stdin);
+        return;
+    }
+}
+
+
+void errorLexico(const int c){
+    printf("[Scanner] Error Lexico\n");
+    printf("[Scanner] Caracter invalido: %c\n",c);
+}
